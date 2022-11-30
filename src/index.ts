ed74c3d3d12ff49a213856cd4a7041474124ecde
YeFei <316606233@qq.com>
2022-11-30 18:02:39 +0800

    ws

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import { Context, Core, SetupFunction } from '@zenweb/core';
+import { WebSocketServer } from 'ws';
 
 export default function setup(): SetupFunction {
   return function websocket(setup) {
